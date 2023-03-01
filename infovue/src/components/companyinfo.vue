4ad0a3236af737de9182af7c1c947fa598b8781d
alexzz521 <58805866+alexzz521@users.noreply.github.com>
2023-03-01 23:01:49 +0800

    code

--- infovue/src/components/companyinfo.vue
+++ infovue/src/components/companyinfo.vue
@@ -1,24 +1,129 @@
 <template>
     <div class="box">
-     <div class="col_news">
-        <div class="col_news_head" frag="面板5">
-            <ul class="col_metas clearfix" frag="窗口5" portletmode="simpleColumnAttri">
+        <div class="col_news">
+            <div class="col_news_head" frag="面板5">
+                <ul class="col_metas clearfix" frag="窗口5" portletmode="simpleColumnAttri">
 
-                <li class="col_title">
-                    <h2>产业大脑</h2>
-                </li>
-                <li class="col_path"><span class="path_name">当前位置：</span><a href="http://127.0.0.1:8080/#/"
-                        target="_self">首页</a><span class='possplit'>&nbsp;&nbsp;</span><a
-                        href="http://127.0.0.1:8080/#/companyinfo" target="_self">产业大脑</a></li>
-            </ul>
+                    <li class="col_title">
+                        <h2>学生创业企业</h2>
+                    </li>
+                    <li class="col_path"><span class="path_name">当前位置：</span><a href="http://127.0.0.1:8080/#/"
+                            target="_self">首页</a><span class='possplit'>&nbsp;&nbsp;</span><a
+                            href="http://127.0.0.1:8080/#/companyinfo" target="_self">学生创业企业</a></li>
+                </ul>
+            </div>
+            <div class="col_news_con" frag="面板6">
+                <div class="col_news_list" frag="窗口6" portletmode="simpleList">
+                    <div class="wp_single wp_column_article" id="wp_column_article">
+                        <div class="wp_entry">
+                            <div class="wp_articlecontent">
+                                <div id="wp_content_w6_0" class="paging_content" style="display:">
+                                    <p>
+                                        <br />
+                                        <br />
+                                    </p>
+                                    <table class="MsoTableGrid" border="1" cellspacing="0" cellpadding="0"
+                                        style="border:none;display: flex;">
+                                        <tbody class="tabd">
+                                            <tr style="mso-yfti-irow:0;mso-yfti-firstrow:yes;height:56px">
+                                                <td width="64"
+                                                    style="width:64px;border:solid #000 1px;mso-border-alt:solid #000 1px;background:#b6dde8;mso-background-themecolor:accent5;mso-background-themetint:102;padding:0cm 7px 0cm 7px;height:56px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <strong>
+                                                            <span
+                                                                style="font-size:16px;font-family:微软雅黑, sans-serif;">序号</span>
+                                                        </strong>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border:solid #000 1px;border-left:none;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;background:#b6dde8;mso-background-themecolor:accent5;mso-background-themetint:102;padding:0cm 7px 0cm 7px;height:56px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <strong>
+                                                            <span
+                                                                style="font-size:16px;font-family:微软雅黑, sans-serif;">公司名称</span>
+                                                        </strong>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border:solid #000 1px;border-left:none;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;background:#b6dde8;mso-background-themecolor:accent5;mso-background-themetint:102;padding:0cm 7px 0cm 7px;height:56px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <strong>
+                                                            <span
+                                                                style="font-size:16px;font-family:微软雅黑, sans-serif;">成立日期</span>
+                                                        </strong>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border:solid #000 1px;border-left:none;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;background:#b6dde8;mso-background-themecolor:accent5;mso-background-themetint:102;padding:0cm 7px 0cm 7px;height:56px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <strong>
+                                                            <span
+                                                                style="font-size:16px;font-family:微软雅黑, sans-serif;">法定代表人</span>
+                                                        </strong>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border:solid #000 1px;border-left:none;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;background:#b6dde8;mso-background-themecolor:accent5;mso-background-themetint:102;padding:0cm 7px 0cm 7px;height:56px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <strong>
+                                                            <span
+                                                                style="font-size:16px;font-family:微软雅黑, sans-serif;">经营范围</span>
+                                                        </strong>
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                            <tr v-for="(value, key) in  tableData" :key=key style="height:53px">
+                                                <td width="64"
+                                                    style="width:64px;border:solid #000 1px;border-top:none;mso-border-top-alt:solid #000 1px;mso-border-alt:solid #000 1px;padding:0cm 7px 0cm 7px;height:53px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <span lang="EN-US" style="font-size:15px;">{{ key + 1 }}</span>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border-top:none;border-left:none;border-bottom:solid #000 1px;border-right:solid #000 1px;mso-border-top-alt:solid #000 1px;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;padding:0cm 7px 0cm 7px;height:53px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <span style="font-size:16px;font-family:微软雅黑, sans-serif;">{{
+                                                            value["公司名称"] }}</span>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border-top:none;border-left:none;border-bottom:solid #000 1px;border-right:solid #000 1px;mso-border-top-alt:solid #000 1px;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;padding:0cm 7px 0cm 7px;height:53px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <span style="font-size:16px;font-family:微软雅黑, sans-serif;">{{
+                                                            value["成立日期"] }}</span>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border-top:none;border-left:none;border-bottom:solid #000 1px;border-right:solid #000 1px;mso-border-top-alt:solid #000 1px;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;padding:0cm 7px 0cm 7px;height:53px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <span style="font-size:16px;font-family:微软雅黑, sans-serif;">{{
+                                                            value["法定代表人"] }}</span>
+                                                    </p>
+                                                </td>
+                                                <td width="406"
+                                                    style="width:406px;border-top:none;border-left:none;border-bottom:solid #000 1px;border-right:solid #000 1px;mso-border-top-alt:solid #000 1px;mso-border-left-alt:solid #000 1px;mso-border-alt:solid #000 1px;padding:0cm 7px 0cm 7px;height:53px">
+                                                    <p class="MsoNormal" style="text-align:center;text-align:center;">
+                                                        <span style="font-size:16px;font-family:微软雅黑, sans-serif;">{{
+                                                            value["经营范围"] }}</span>
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <p>
+                                        <br />
+                                    </p>
+                                    <p>
+                                        <br />
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    <div class="staticdiv">
-        <!-- <button @click="kuayu">"跨域请求"</button> -->
-        <el-table :data="tableData"  style="width: 100%" >
-    <el-table-column v-for='item in  data_cloums' :key="item" :prop="item" :label="item" width="181"/></el-table>
     </div>
-</div>
-</div>
 </template>
 
 <script>
@@ -27,7 +132,7 @@ export default {
     name: 'companyinfoP',
     data() {
         return {
-            data_cloums:[
+            data_cloums: [
                 "序号",
                 "公司名称",
                 "成立日期",
@@ -38,21 +143,21 @@ export default {
                 "注册地址",
                 "经营范围"
             ],
-            tableData:[]
+            tableData: []
         }
     },
-/*     methods: { */
-    created () {
-      this.$axios.get('/demo/company1')
-        .then(res => {
-          this.tableData = JSON.parse(res.data)
-          console.log(this.tableData)
-          console.log(typeof(this.tableData))
-        })
-        .catch(err => {
-          console.log(err)
-        })
-  }
+    /*     methods: { */
+    created() {
+        this.$axios.get('/demo/company1')
+            .then(res => {
+                this.tableData = JSON.parse(res.data)
+                console.log(this.tableData)
+                console.log(typeof (this.tableData))
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
 }
 </script>
@@ -62,4 +167,5 @@ export default {
     width: 100%;
     display: flex;
     justify-content: center
-}</style>
+}
+</style>
--- infovue/src/components/homevue.vue
+++ infovue/src/components/homevue.vue
@@ -2,20 +2,48 @@
     <div id="home" class="Home">
         <table class="hometable">
             <tr>
-                <th class="class1"><a href="http://127.0.0.1/ny/#/" style="color:black; text-decoration: none;">园区概况</a>
-                </th>
-                <th class="class1"><router-link to="/Info" style="color:black; text-decoration: none;">信息统计</router-link>
-                </th>
                 <th class="class1">
-                    <router-link to="/company1" style="color:black; text-decoration: none;">企业</router-link>
-                </th>
+            <tr>
+                <img  class="img1" src="../pictures/introduction.jpg">
             </tr>
             <tr>
-                <th class="class1"><router-link to="/companyinfo"
-                        style="color:black; text-decoration: none;">产业大脑</router-link></th>
-                <th class="class1"><router-link to="/keji"
-                        style="color:black; text-decoration: none;">科技动态</router-link></th>
-                <th class="class1"><a href=" " style="color:black; text-decoration: none;">产学研合作</a></th>
+                <a href="http://127.0.0.1/ny/#/" style="color:black; text-decoration: none;">园区概况</a>
+            </tr>
+            </th>
+            <th class="class1">
+                <tr>
+                    <img  class="img1" src="../pictures/information.jpg">
+                </tr>
+                <tr>
+                    <router-link to="/Info" style="color:black; text-decoration: none;">信息统计</router-link>
+                </tr>
+            </th>
+            <th class="class1">
+                <tr>
+                    <img  class="img1" src="../pictures/company.jpg">
+                </tr>
+                <tr><router-link to="/company1" style="color:black; text-decoration: none;">企业</router-link></tr>
+            </th>
+            </tr>
+            <tr>
+                <th class="class1">
+            <tr>
+                <img  class="img1" src="../pictures/brain.jpg">
+            </tr>
+            <tr><router-link to="/companyinfo" style="color:black; text-decoration: none;">产业大脑</router-link></tr>
+            </th>
+            <th class="class1">
+                <tr>
+                    <img  class="img1" src="../pictures/police.jpg">
+                </tr>
+                <tr><router-link to="/keji" style="color:black; text-decoration: none;">科技动态</router-link></tr>
+            </th>
+            <th class="class1">
+                <tr>
+                    <img  class="img1" src="../pictures/chanxueyan.jpg">
+                </tr>
+                <tr><a href=" " style="color:black; text-decoration: none;">产学研合作</a></tr>
+            </th>
             </tr>
         </table>
     </div>
@@ -44,25 +72,24 @@ export default {
     position: absolute;
 } */
 
-.Home{
+.Home {
     width: 100%;
     height: 100%;
 }
 
+.img1{
+    width:200px;
+    height: 100px;
+}
 .class1 {
     font: 12px/1.5 Tahoma, Helvetica, Arial, '宋体', sans-serif;
     font-size: 35px;
     font-weight: 1000;
     color: black;
-    border: solid;
-    border-width: thick;
-    border-style: outset;
-    border-collapse: separate;
-
 }
 
 .hometable {
-    border-spacing: 100px;
+    border-spacing: 20px;
     height: 100%;
     width: 100%;
 }
